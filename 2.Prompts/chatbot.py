89d716f8a9931965848c44d7d82c133e589b5d08
Mahit <mahitraj2004@gmail.com>
2025-08-09 22:50:18 +0545

    Changes Made

--- 2.Prompts/chatbot.py
+++ 2.Prompts/chatbot.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI()
+model = ChatGoogleGenerativeAI()
 
 chat_history = [
     SystemMessage(content='You are a helpful AI assistant')
